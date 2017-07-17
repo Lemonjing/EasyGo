793f5b246d4ed8c3589033ebc20e929b9dbdcdbd
Ryan Tao <xmusaber@163.com>
2017-07-17 08:31:20 +0800

    Add

--- main.go
+++ main.go
@@ -2,26 +2,10 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
-	"database/sql"
+	"./api"
 )
 
 func main() {
-	fmt.Println("==========")
-	db, err := sql.Open("mysql", "root:root@/golang")
-	checkErr(err)
-
-	stmt, err := db.Prepare(`INSERT user (user_name,user_age,user_sex) values (?,?,?)`)
-	checkErr(err)
-	res, err := stmt.Exec("tony", 20, 1)
-	checkErr(err)
-	id, err := res.LastInsertId()
-	checkErr(err)
-	fmt.Println("id=", id)
-}
-
-func checkErr(err error) {
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("+++++")
+	api.Mysql()
 }
